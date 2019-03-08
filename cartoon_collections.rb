b3f232eac35453811be61c2119d6c23d77521689
Eric Storey <estorey@gmail.com>
2019-03-08 23:54:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names) # code an argument here
+  names.each do |name|
+    puts name
+  end 
 end
 
 def summon_captain_planet# code an argument here
